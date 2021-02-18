918149a156196e71e0f41e70c4121cfb808b488d
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-02-18 19:14:17 +0200

    Made project title and desc to switch on button click

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-// PROJECT VARIABLES
+// PROJECTS 
+
+// Constants
+const PROJECT_ONE_TITLE = "Recipe Search";
+const PROJECT_TWO_TITLE = "Shopping Site";
+const PROJECT_THREE_TITLE = "Tic Tac Toe";
+
+const PROJECT_ONE_DESC = "First one"
+const PROJECT_TWO_DESC = "Second one"
+const PROJECT_THREE_DESC = "Third one"
+
+// Project Variables
 const projectSection = document.querySelector(".projects-section");
 const projectContainer = document.querySelector(".projects-container");
 const projectTitle = document.querySelector(".projects-title");
 const projectImg = document.querySelector(".project-img");
-const projectStackImg = document.querySelectorAll(".project-stack-img");
+const projectStackImgs = document.querySelectorAll(".project-stack-img");
 const projectName = document.querySelector(".project-name");
 const projectDescription = document.querySelector(".project-description");
-const projectBtn = document.querySelector(".project-choice");
+const projectBtns = document.querySelectorAll(".project-choice");
+const projectOneBtn = document.querySelector("#projectOne");
+const projectTwoBtn = document.querySelector("#projectTwo");
+const projectThreeBtn = document.querySelector("#projectThree");
 const viewBtn = document.querySelector("#viewBtn");
 const codeBtn = document.querySelector("#codeBtn");
+
 
 // Moving Animation
 projectContainer.addEventListener("mousemove", (e) => {
@@ -22,11 +37,13 @@ projectContainer.addEventListener("mouseenter", (e) => {
    projectContainer.style.transition = "none";
    //Popout
    projectTitle.style.transform ="translateZ(150px)";
-   projectStackImg.forEach(stackImg => {
+   projectStackImgs.forEach(stackImg => {
    stackImg.style.transform ="translateZ(200px)";
    });
    projectDescription.style.transform ="translateZ(125px)";
-   projectBtn.style.transform ="translateZ(100px)";
+   projectBtns.forEach(btn => {
+   btn.style.transform ="translateZ(100px)";
+   });
    viewBtn.style.transform ="translateZ(75px)";
    codeBtn.style.transform ="translateZ(75px)";
 });
@@ -36,11 +53,33 @@ projectContainer.addEventListener("mouseleave", (e) => {
    projectContainer.style.transform = `rotateY(0deg) rotateX(0deg)`;
    //Popback
    projectTitle.style.transform ="translateZ(0px)";
-   projectStackImg.forEach(stackImg => {
+   projectStackImgs.forEach(stackImg => {
    stackImg.style.transform ="translateZ(0px) rotateZ(0deg)";
    });
    projectDescription.style.transform ="translateZ(0px)";
-   projectBtn.style.transform ="translateZ(0px)";
+   projectBtns.forEach(btn => {
+   btn.style.transform ="translateZ(0px)";
+   });
    viewBtn.style.transform ="translateZ(0px)";
    codeBtn.style.transform ="translateZ(0px)";
 });
+
+// Switch Projects
+const projectOneChange = () => {
+   projectName.innerHTML = PROJECT_ONE_TITLE;
+   projectDescription.innerHTML = PROJECT_ONE_DESC;
+}
+
+const projectTwoChange = () => {
+   projectName.innerHTML = PROJECT_TWO_TITLE;
+   projectDescription.innerHTML = PROJECT_TWO_DESC;
+}
+
+const projectThreeChange = () => {
+   projectName.innerHTML = PROJECT_THREE_TITLE;
+   projectDescription.innerHTML = PROJECT_THREE_DESC;
+}
+
+projectOneBtn.addEventListener("click",projectOneChange);
+projectTwoBtn.addEventListener("click",projectTwoChange);
+projectThreeBtn.addEventListener("click",projectThreeChange);
